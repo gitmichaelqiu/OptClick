eac183ef7da594b969c19c552127eb22d9393565
michaelqiu <altmickymike@outlook.com>
2025-09-18 18:30:00 +0800

    Initial Commit

---
+++ OptClick/OptClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptClickApp.swift
+//  OptClick
+//
+//  Created by Michael Qiu on 2025/9/18.
+//
+
+import SwiftUI
+
+@main
+struct OptClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OptClickTests/OptClickTests.swift
@@ -0,0 +1,17 @@
+//
+//  OptClickTests.swift
+//  OptClickTests
+//
+//  Created by Michael Qiu on 2025/9/18.
+//
+
+import Testing
+@testable import OptClick
+
+struct OptClickTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
