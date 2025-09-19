8e55fef4a8e4c0f4d0b84d27b7fc6703d3a0ac19
michaelqiu <altmickymike@outlook.com>
2025-09-19 09:41:18 +0800

    Add update options in GeneralSettings

--- OptClick/Views/GeneralSettingsView.swift
+++ OptClick/Views/GeneralSettingsView.swift
@@ -2,10 +2,20 @@ import SwiftUI
 
 struct GeneralSettingsView: View {
     @EnvironmentObject var inputManager: InputManager
+    @State private var autoCheckForUpdates = UpdateManager.isAutoCheckEnabled
 
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
             Toggle("Enable Option → Right Click", isOn: $inputManager.isEnabled)
+            
+            Toggle("Automatically check for updates", isOn: $autoCheckForUpdates)
+                .onChange(of: autoCheckForUpdates) {
+                    UpdateManager.isAutoCheckEnabled = autoCheckForUpdates
+                }
+            
+            Button("Check for Updates") {
+                UpdateManager.shared.checkForUpdate(from: nil)
+            }
             
             Spacer()
         }
