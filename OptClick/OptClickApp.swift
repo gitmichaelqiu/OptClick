e6899733d2f9f5a250ccfb86bf5cf8873636141d
michaelqiu <altmickymike@outlook.com>
2025-09-21 23:03:20 +0800

    Add menu bar item and menu

--- OptClick/OptClickApp.swift
+++ OptClick/OptClickApp.swift
@@ -1,26 +1,56 @@
 import SwiftUI
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
+    var statusItem: NSStatusItem?
+    var popover = NSPopover()
+    
+    @StateObject private var inputManager = InputManager()
+    @StateObject private var hotkeyManager = HotkeyManager()
+    
+    @objc func showSettings() {
+        guard let statusBarButton = statusItem?.button else { return }
+        popover.show(relativeTo: statusBarButton.bounds, of: statusBarButton, preferredEdge: .maxY)
+    }
+    
+    func addMenuItems() {
+        statusItem?.menu?.removeAllItems()
+        statusItem?.menu?.addItem(withTitle: "Settings...", action: #selector(showSettings), keyEquivalent: "")
+    }
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusItem?.button?.title = "OptClick"
+        
+        statusItem?.menu = NSMenu()
+        statusItem?.menu?.delegate = self
+    }
+    
+    func menuWillOpen(_ menu: NSMenu) {
+        addMenuItems()
+    }
+}
 
 @main
 struct OptClickApp: App {
-    @StateObject private var inputManager = InputManager()
-    @StateObject private var hotkeyManager = HotkeyManager()
-
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
-        WindowGroup {
-            SettingsView()
-                .environmentObject(inputManager)
-                .environmentObject(hotkeyManager)
-                .frame(width: 450, height: 450)
-                .onAppear {
-                    NotificationCenter.default.addObserver(
-                        forName: .hotkeyTriggered,
-                        object: nil,
-                        queue: .main
-                    ) { _ in
-                        inputManager.isEnabled.toggle()
-                    }
-                }
-        }
-        .windowResizability(.contentSize)
+//        WindowGroup {
+//            SettingsView()
+//                .environmentObject(inputManager)
+//                .environmentObject(hotkeyManager)
+//                .frame(width: 450, height: 450)
+//                .onAppear {
+//                    NotificationCenter.default.addObserver(
+//                        forName: .hotkeyTriggered,
+//                        object: nil,
+//                        queue: .main
+//                    ) { _ in
+//                        inputManager.isEnabled.toggle()
+//                    }
+//                }
+//        }
+//        .windowResizability(.contentSize)
     }
 }
