cd201777f9b68e0e0497333fc25208ee4603bdcc
michaelqiu <altmickymike@outlook.com>
2025-09-22 00:02:29 +0800

    Update custom menu bar icon

--- OptClick/OptClickApp.swift
+++ OptClick/OptClickApp.swift
@@ -106,12 +106,73 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
             name: .hotkeyTriggered,
             object: nil
         )
+        
+        updateStatusBarIcon()
     }
     
     @objc private func handleHotkeyTriggered() {
         inputManager.isEnabled.toggle()
+        updateStatusBarIcon()
     }
     
+    // Menu bar icon
+    private var cachedEnabledIcon: NSImage?
+    private var cachedDisabledIcon: NSImage?
+
+    func updateStatusBarIcon() {
+        guard let button = statusItem?.button else { return }
+
+        let icon: NSImage
+        if inputManager.isEnabled {
+            icon = cachedEnabledIcon ?? makeOptionIcon()
+            cachedEnabledIcon = icon
+        } else {
+            icon = cachedDisabledIcon ?? makeOptionWithSlashIcon()
+            cachedDisabledIcon = icon
+        }
+
+        button.image = icon
+    }
+
+    private func makeOptionIcon() -> NSImage {
+        let size = NSSize(width: 15, height: 15)
+        let image = NSImage(systemSymbolName: "option", accessibilityDescription: "Option")!
+        let resized = resizeImage(image, to: size)
+        resized.isTemplate = true
+        return resized
+    }
+
+    private func makeOptionWithSlashIcon() -> NSImage {
+        let size = NSSize(width: 15, height: 15)
+
+        let combinedImage = NSImage(size: size)
+        combinedImage.lockFocus()
+
+        // Draw option
+        if let optionImage = NSImage(systemSymbolName: "option", accessibilityDescription: nil) {
+            let resizedOption = resizeImage(optionImage, to: size)
+            resizedOption.draw(in: NSRect(origin: .zero, size: size))
+        }
+
+        // Draw slash
+        let path = NSBezierPath()
+        path.move(to: NSPoint(x: 2, y: 1))
+        path.line(to: NSPoint(x: size.width-2, y: size.height-1))
+        path.lineWidth = 2
+        path.stroke()
+
+        combinedImage.unlockFocus()
+        combinedImage.isTemplate = true
+        return combinedImage
+    }
+
+    private func resizeImage(_ image: NSImage, to size: NSSize) -> NSImage {
+        let scaled = NSImage(size: size)
+        scaled.lockFocus()
+        image.draw(in: NSRect(origin: .zero, size: size), from: NSRect(origin: .zero, size: image.size), operation: .copy, fraction: 1.0)
+        scaled.unlockFocus()
+        return scaled
+    }
 }
 
 @main
