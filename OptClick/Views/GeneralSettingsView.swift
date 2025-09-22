2effb78c49b6a1090e4e668872c69429def9d401
michaelqiu <altmickymike@outlook.com>
2025-09-22 12:48:10 +0800

    Add launch at login

--- OptClick/Views/GeneralSettingsView.swift
+++ OptClick/Views/GeneralSettingsView.swift
@@ -3,10 +3,16 @@ import SwiftUI
 struct GeneralSettingsView: View {
     @ObservedObject var inputManager: InputManager
     @State private var autoCheckForUpdates = UpdateManager.isAutoCheckEnabled
+    @State private var launchAtLogin = LaunchManager.isEnabled
 
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
             Toggle(NSLocalizedString("Settings.General.Option.Enable", comment: "Enable option to right click"), isOn: $inputManager.isEnabled)
+            
+            Toggle(NSLocalizedString("Settings.General.Launch", comment: "Launch at login"), isOn: $launchAtLogin)
+                .onChange(of: launchAtLogin) {
+                    LaunchManager.setEnabled(launchAtLogin)
+                }
             
             Toggle(NSLocalizedString("Settings.General.Update.AutoCheck", comment: "Automatically check for updates"), isOn: $autoCheckForUpdates)
                 .onChange(of: autoCheckForUpdates) {
